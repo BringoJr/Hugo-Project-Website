dc5492f143ccaa8add5321bfddb5e8b8f20ccdee
BringoJr <hwb4547@nyu.edu>
2025-03-17 17:35:26 +0000

    .NET setup

---
+++ my-dotnet-webpage/Models/HomeModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MyDotNetWebpage.Models
+{
+    public class HomeModel
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
